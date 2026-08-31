7b9aeb17854a8821001fa7e4973a50341e1376fa
Kevin Baca <github@aidiastudios.com>
2026-08-31 12:02:23 -0700

    Add Session 1 reference game

---
+++ reference-games/session-1/core/src/main/java/org/ftcgame/RobotGame.java
@@ -0,0 +1,74 @@
+package org.ftcgame;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class RobotGame implements ApplicationListener {
+    private SpriteBatch batch;
+    private Texture robotTexture;
+
+    private float robotX = 100;
+    private float robotY = 100;
+    private float robotSpeed = 3;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        robotTexture = new Texture("robot.png");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
+        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
+        if(width <= 0 || height <= 0) return;
+
+        // Resize your application here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.1f, 0.1f, 0.15f, 1);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            robotY = robotY + robotSpeed;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            robotY = robotY - robotSpeed;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            robotX = robotX - robotSpeed;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            robotX = robotX + robotSpeed;
+        }
+
+        batch.begin();
+        batch.draw(robotTexture, robotX, robotY);
+        batch.end();
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        robotTexture.dispose();
+    }
+}
